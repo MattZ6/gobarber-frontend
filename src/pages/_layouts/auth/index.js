b349d4409398c643dc21f151e677e1814f0410ea
Matheus Zanin <matt_z6@hotmail.com>
2019-12-26 16:41:26 -0300

    chore: SingIn and SignUp pages styles

--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper } from '~/pages/_layouts/auth/styles';
+import { Wrapper, Content } from '~/pages/_layouts/auth/styles';
 
 export default function AuthLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
 }
 
 AuthLayout.propTypes = {
