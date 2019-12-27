67502c47f280a0a51354a0c1931a8f76d3658c80
Matheus Zanin <matt_z6@hotmail.com>
2019-12-27 11:08:24 -0300

    chore: Loading and toast into login action

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -9,15 +9,26 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case ActionTypes.SIGN_IN_SUCCESS:
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case ActionTypes.SIGN_IN_REQUEST: {
+        draft.loading = true;
+        break;
+      }
+
+      case ActionTypes.SIGN_IN_SUCCESS: {
         draft.token = action.payload.token;
         draft.signed = true;
         draft.loading = false;
-      });
+        break;
+      }
 
-    default:
-      return state;
-  }
+      case ActionTypes.SIGN_FAILURE: {
+        draft.loading = false;
+        break;
+      }
+
+      default:
+    }
+  });
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,21 +1,28 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from '~/store/modules/auth/actions';
+import { signInSuccess, signFailure } from '~/store/modules/auth/actions';
 import ActionTypes from '~/store/modules/auth/types';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const { data } = yield call(api.post, 'sessions', { email, password });
+    const { data } = yield call(api.post, 'sessions', { email, password });
 
-  const { token } = data;
+    const { token } = data;
 
-  yield put(signInSuccess(token));
+    yield put(signInSuccess(token));
 
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
+
+    yield put(signFailure());
+  }
 }
 
 export default all([takeLatest(ActionTypes.SIGN_IN_REQUEST, signIn)]);
