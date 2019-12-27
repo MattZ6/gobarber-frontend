354ddc8737909fc219e77a73337a7d2c1f543923
Matheus Zanin <matt_z6@hotmail.com>
2019-12-27 16:59:37 -0300

    chore: Dashboard style

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,41 @@
-import React, { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  // const dispatch = useDispatch();
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
 
-  useEffect(() => {}, []);
-
-  return <h1>Dashboard</h1>;
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Nome do cliente</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Nome do cliente</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Nome do cliente</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 600px;
+  margin: 30px auto 60px;
+
+  header {
+    display: flex;
+    align-self: center;
+    align-items: center;
+
+    button {
+      border: 0;
+      background: none;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
+  box-shadow: ${props => (props.past ? 'none' : '2px 2px 3px rgba(0,0,0,.2)')};
+
+  strong {
+    display: block;
+    color: ${props => (props.available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${props => (props.available ? '#999' : '#666')};
+  }
+`;
